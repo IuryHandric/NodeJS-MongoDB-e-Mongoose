def93bf5cacf919892544b5a3f1c8511c6c1800f
Iury Handric <handric11@hotmail.com>
2025-06-24 11:37:33 -0300

    Inserindo valores no banco de dados com mongoose

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,71 +1,74 @@
 const Product = require('../models/Product')
 
 module.exports = class ProductController {
-    static async showProducts(req, res) {
+    // static async showProducts(req, res) {
 
-        const products = await Product.getProducts()
+    //     const products = await Product.getProducts()
 
-        res.render('products/all', { products })
-    }
+    //     res.render('products/all', { products })
+    // }
 
     static createProduct(req, res) {
         res.render('products/create')
     }
 
-    static createProductPost(req, res) {
+    static async createProductPost(req, res) {
         const name = req.body.name
         const image = req.body.image
         const price = req.body.price
         const description = req.body.description
 
-        const product = new Product(name, image, price, description)
 
-        product.save()
+        // Passando os parâmetros como objeto
+        const product = new Product({name, image, price, description})
+
+        await product.save()
+        
         console.log(`Produto [${name}] criado com sucesso!`)
 
         res.redirect('/products')
     }
 
-    static async getProduct(req, res) {
-        const id = req.params.id
+//     static async getProduct(req, res) {
+//         const id = req.params.id
 
-        const product = await Product.getProductById(id)
+//         const product = await Product.getProductById(id)
 
-        res.render('products/product', { product })
+//         res.render('products/product', { product })
 
-    }
+//     }
 
-    static async removeProduct(req, res) {
-        const id = req.params.id
+//     static async removeProduct(req, res) {
+//         const id = req.params.id
 
-        await Product.removeProductById(id)
-        console.log('Produto exluído com sucesso!')
-        res.redirect('/products')
+//         await Product.removeProductById(id)
+//         console.log('Produto exluído com sucesso!')
+//         res.redirect('/products')
 
-    }
+//     }
 
-    static async editProduct(req, res) {
-        const id = req.params.id
+//     static async editProduct(req, res) {
+//         const id = req.params.id
 
-        const product = await Product.getProductById(id)
+//         const product = await Product.getProductById(id)
 
-        res.render('products/edit', { product })
-    }
+//         res.render('products/edit', { product })
+//     }
 
-    static async editProductPost(req, res) {
-        const id = req.body.id
-        const name = req.body.name
-        const image = req.body.image
-        const price = req.body.price
-        const description = req.body.description
+//     static async editProductPost(req, res) {
+//         const id = req.body.id
+//         const name = req.body.name
+//         const image = req.body.image
+//         const price = req.body.price
+//         const description = req.body.description
 
-        const product = new Product(name, image, price, description);
+//         const product = new Product(name, image, price, description);
 
-        await product.updateProduct(id)
+//         await product.updateProduct(id)
         
-        console.log('Produto editado com sucesso!')
-        res.redirect('/products')
+//         console.log('Produto editado com sucesso!')
+//         res.redirect('/products')
 
-    }
+//     }
 
 }
